19643fd205892fe0befeef43c2f3161b681d494b
Erik Hoyjor <ErikHoyJor@gmail.com>
2016-09-27 13:59:31 +0200

    readied the program for GpggaMeasurement class

--- app/src/main/java/dk/au/cs/pervasivepositioninggps/MainActivity.java
+++ app/src/main/java/dk/au/cs/pervasivepositioninggps/MainActivity.java
@@ -1,26 +1,17 @@
 package dk.au.cs.pervasivepositioninggps;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.GnssClock;
-import android.location.GnssMeasurementsEvent;
-import android.location.GnssMeasurementsEvent.Callback;
 import android.location.LocationManager;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.location.GnssMeasurement;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
-
-import java.net.URISyntaxException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,8 +19,6 @@ public class MainActivity extends AppCompatActivity {
     NmeaMonitor m_Monitor;
 
     public void onGetFixClicked(View view) {
-        //m_LocMan.getAllProviders();
-
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             Log.e("Location", "Could not shit");
             return;
@@ -38,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(getApplicationContext(), this.getClass());
 
         PendingIntent pi = PendingIntent.getBroadcast(getApplicationContext(), 0, intent,
-                PendingIntent.FLAG_UPDATE_CURRENT);
+                PendingIntent.FLAG_CANCEL_CURRENT);
 
         m_LocMan.requestSingleUpdate(LocationManager.GPS_PROVIDER, pi);
     }
@@ -51,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             Log.e("Initialize", "Failed to acquire the proper permissions");
-            //return;
+            //return; // we (currently) want the stack trace if this fails.
         }
         m_Monitor = new NmeaMonitor();
         if(m_LocMan.addNmeaListener(m_Monitor))
@@ -63,14 +52,4 @@ public class MainActivity extends AppCompatActivity {
             Log.e("Initialize", "Failed started listener for NMEA");
         }
     }
-
-//    @SuppressLint("NewApi")
-//    private class GnssCallback extends GnssMeasurementsEvent.Callback {
-//        @Override
-//        public void onGnssMeasurementsReceived(GnssMeasurementsEvent eventArgs)
-//        {
-//            Toast.makeText(getApplicationContext(), eventArgs.toString(), Toast.LENGTH_SHORT);
-//            Log.i("Location", eventArgs.toString());
-//        }
-//    }
 }
--- app/src/main/java/dk/au/cs/pervasivepositioninggps/NmeaMonitor.java
+++ app/src/main/java/dk/au/cs/pervasivepositioninggps/NmeaMonitor.java
@@ -2,14 +2,25 @@ package dk.au.cs.pervasivepositioninggps;
 
 import android.location.GpsStatus.NmeaListener;
 import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by Erik on 27-09-2016.
  */
 
 public class NmeaMonitor implements NmeaListener {
+    public ArrayList<GpggaMeasurement> measurements = new ArrayList<GpggaMeasurement>();
     @Override
     public void onNmeaReceived(long timestamp, String nmea) {
-        Log.i("Localization", timestamp + ": " + nmea);
+        if (!nmea.startsWith("$GPGGA")) {
+            return;
+        }
+
+        GpggaMeasurement fix = GpggaMeasurement.parseString(nmea);
+
+        if (fix.isValid()) {
+            Log.i("Localization", timestamp + ": " + nmea);
+            measurements.add(fix);
+        }
     }
 }
