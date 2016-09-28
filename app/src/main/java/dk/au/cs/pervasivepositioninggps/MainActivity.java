2a5c31c9f8c5155607a097a9285a56cf5d56f17b
Erik Hoyjor <ErikHoyJor@gmail.com>
2016-09-28 12:01:22 +0200

    Remade layout

--- app/src/main/java/dk/au/cs/pervasivepositioninggps/MainActivity.java
+++ app/src/main/java/dk/au/cs/pervasivepositioninggps/MainActivity.java
@@ -1,21 +1,64 @@
 package dk.au.cs.pervasivepositioninggps;
 
 import android.Manifest;
-import android.app.PendingIntent;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.LocationManager;
+import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean m_Started = false;
+
     LocationManager m_LocMan;
     NmeaMonitor m_Monitor;
+
+    RadioGroup m_RadioGroup;
+    EditText m_ValueField;
+    Button m_StartButton;
+
+    public void onStartBtnClicked(View view)
+    {
+        switch (m_RadioGroup.getCheckedRadioButtonId())
+        {
+            case R.id.trigger1:
+                break;
+            case R.id.trigger2:
+                break;
+            case R.id.trigger3:
+                break;
+            case R.id.trigger4:
+                break;
+            default:
+                Toast.makeText(getApplicationContext(), "Error: No trigger type was selected", Toast.LENGTH_SHORT).show();
+                return;
+        }
+        m_Started = true;
+        setEnableRadioButtons(false);
+        m_StartButton.setEnabled(false);
+    }
+
+    public void onSaveBtnClicked(View view)
+    {
+        m_Started = false;
+        Toast.makeText(getApplicationContext(), "File saved", Toast.LENGTH_SHORT).show();
+        m_StartButton.setEnabled(true);
+        setEnableRadioButtons(true);
+        m_RadioGroup.clearCheck();
+    }
+
+    private void setEnableRadioButtons(boolean enable)
+    {
+        for(int i = 0; i < m_RadioGroup.getChildCount(); i++){
+            m_RadioGroup.getChildAt(i).setEnabled(enable);
+        }
+        m_RadioGroup.setEnabled(enable);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,5 +79,9 @@ public class MainActivity extends AppCompatActivity {
         {
             Log.e("Initialize", "Failed started listener for NMEA");
         }
+
+        m_RadioGroup = (RadioGroup)findViewById(R.id.radioGroup);
+        m_ValueField = (EditText) findViewById(R.id.inputField);
+        m_StartButton = (Button) findViewById(R.id.buttonStart);
     }
 }
