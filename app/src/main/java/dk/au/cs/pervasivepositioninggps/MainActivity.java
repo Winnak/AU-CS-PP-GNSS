2c59bec2e04ab07720f4f3a237c032b1c72cb7b1
Erik Hoyjor <ErikHoyJor@gmail.com>
2016-09-28 10:54:41 +0200

    removed button. refactored program, moved stuff to nmea monitor.

--- app/src/main/java/dk/au/cs/pervasivepositioninggps/MainActivity.java
+++ app/src/main/java/dk/au/cs/pervasivepositioninggps/MainActivity.java
@@ -17,20 +17,6 @@ public class MainActivity extends AppCompatActivity {
     LocationManager m_LocMan;
     NmeaMonitor m_Monitor;
 
-    public void onGetFixClicked(View view) {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            Log.e("Location", "Could not shit");
-            return;
-        }
-
-        Intent intent = new Intent(getApplicationContext(), this.getClass());
-
-        PendingIntent pi = PendingIntent.getBroadcast(getApplicationContext(), 0, intent,
-                PendingIntent.FLAG_CANCEL_CURRENT);
-
-        m_LocMan.requestSingleUpdate(LocationManager.GPS_PROVIDER, pi);
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
             Log.e("Initialize", "Failed to acquire the proper permissions");
             //return; // we (currently) want the stack trace if this fails.
         }
-        m_Monitor = new NmeaMonitor();
+        m_Monitor = new NmeaMonitor(m_LocMan, getApplicationContext());
         if(m_LocMan.addNmeaListener(m_Monitor))
         {
             Log.i("Initialize", "Successfully started listener for NMEA");
--- app/src/main/java/dk/au/cs/pervasivepositioninggps/NmeaMonitor.java
+++ app/src/main/java/dk/au/cs/pervasivepositioninggps/NmeaMonitor.java
@@ -1,7 +1,15 @@
 package dk.au.cs.pervasivepositioninggps;
 
+import android.Manifest;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.GpsStatus.NmeaListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
+
 import java.util.ArrayList;
 
 /**
@@ -10,6 +18,35 @@ import java.util.ArrayList;
 
 public class NmeaMonitor implements NmeaListener {
     public ArrayList<GpggaMeasurement> measurements = new ArrayList<GpggaMeasurement>();
+
+    private LocationManager m_LocMan;
+    private Context m_AppContext;
+
+    public NmeaMonitor(LocationManager locman, Context appContext) {
+        this.m_LocMan = locman;
+        this.m_AppContext = appContext;
+    }
+
+    public void getFix() {
+        Intent intent = new Intent(m_AppContext, this.getClass());
+
+        PendingIntent pi = PendingIntent.getBroadcast(m_AppContext, 0, intent,
+                PendingIntent.FLAG_CANCEL_CURRENT);
+
+        if (ActivityCompat.checkSelfPermission(m_AppContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(m_AppContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Log.e("NMEA Monitor", "Could not find shit");
+            return;
+        }
+        m_LocMan.requestSingleUpdate(LocationManager.GPS_PROVIDER, pi);
+    }
+
     @Override
     public void onNmeaReceived(long timestamp, String nmea) {
         if (!nmea.startsWith("$GPGGA")) {
